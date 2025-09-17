e3ca6ad6dd3f530248144b02e585a93230529db0
H.M Nadun Tilan Weerasinghe <74712552+NADUN-TILAN@users.noreply.github.com>
2025-09-17 22:59:32 +0530

    Add prompt parameter to GitHub and Google authentication providers to allow account selection during login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -34,6 +34,11 @@ if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "select_account"
+        }
+      }
     })
   );
 }
@@ -43,6 +48,11 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "select_account"
+        }
+      }
     })
   );
 }
